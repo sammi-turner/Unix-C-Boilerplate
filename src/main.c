dc02582132d03a34eb1e9b0ad26582d8a7db1cfb
Sammi Turner <ms.sammi.turner@gmail.com>
2024-06-29 14:34:57 +0100

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 #include "interface.h"
 
-int main(int argc, char **argv)
+int main() 
 {
+    // Set locale for wide character output
+    setlocale(LC_ALL, "");
+
+    // append array of wide chars to a file
+    append_wchar_to_file("nerds.txt", L"Hello nerds! 🤓 👋🏻");
     return 0;
 }
